ab2a817e4674723e275b1db6f4385bdcbad47f5b
Taras Keryk <tarasx.keryk@intel.com>
2022-05-10 00:29:22 -0700

    fixed test
    Signed-off-by: Taras Keryk <tarasx.keryk@intel.com>

--- tests/show_barefoot_test.py
+++ tests/show_barefoot_test.py
@@ -1,32 +1,12 @@
 import os
-import sys
-import textwrap
-from unittest import mock
-
 import pytest
+import subprocess
 from click.testing import CliRunner
 
-test_path = os.path.dirname(os.path.abspath(__file__))
-modules_path = os.path.dirname(test_path)
-sys.path.insert(0, modules_path)
 import show.main as show
+from .utils import get_result_and_return_code
 
-
-@pytest.fixture(scope='class')
-def config_env():
-    os.environ["UTILITIES_UNIT_TESTING"] = "1"
-
-    yield
-
-    os.environ["UTILITIES_UNIT_TESTING"] = "0"
-
-class TestShowPlatformBarefoot(object):
-    """
-        Note: `show platform barefoot`  Here we test that the utility is called
-        with the appropriate option(s). 
-    """
-    def test_barefoot(self):
-        with mock.patch('utilities_common.cli.run_command') as mock_run_command:
-            CliRunner().invoke(show.cli.commands['platform'].commands['barefoot'].commands['profile'], [])
-        assert mock_run_command.call_count == 1
-        mock_run_command.assert_called_with('show platform barefoot', display_cmd=False)
+class TestShowBarefoot(object):
+    def test_show_platform_barefoot_profile(self):
+        return_code, result = get_result_and_return_code("show platform bareboot profile")
+        assert return_code == 0
