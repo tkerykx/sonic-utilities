9cb02c13d47d6c9c1ec679035feb48cb1fcc16b9
Taras Keryk <tarasx.keryk@intel.com>
2022-05-10 01:25:44 -0700

    fix test..
    Signed-off-by: Taras Keryk <tarasx.keryk@intel.com>

--- tests/show_barefoot_test.py
+++ tests/show_barefoot_test.py
@@ -8,5 +8,9 @@ from .utils import get_result_and_return_code
 
 class TestShowBarefoot(object):
     def test_show_platform_barefoot_profile(self):
-        return_code, result = get_result_and_return_code("show platform barefoot profile")
+        return_code, output = get_result_and_return_code("show platform barefoot profile")
         assert return_code == 127
+        opts = 'None'
+        if (output.contains("_tofino")):
+            opts = output
+        print(opts)
