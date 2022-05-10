6c9e7f3a1c09b1b51f09db49b28c604e0aee779e
Taras Keryk <tarasx.keryk@intel.com>
2022-05-10 08:35:41 -0700

    tests
    Signed-off-by: Taras Keryk <tarasx.keryk@intel.com>

--- tests/show_barefoot_test.py
+++ tests/show_barefoot_test.py
@@ -1,16 +1,31 @@
 import os
+import sys
+import textwrap
+from unittest import mock
+
 import pytest
-import subprocess
 from click.testing import CliRunner
 
+test_path = os.path.dirname(os.path.abspath(__file__))
+modules_path = os.path.dirname(test_path)
+sys.path.insert(0, modules_path)
 import show.main as show
-from .utils import get_result_and_return_code
 
-class TestShowBarefoot(object):
-    def test_profile(self):
-        return_code, output = get_result_and_return_code("show platform barefoot profile")
-        assert return_code == 127
-        opts = 'None'
-        if "_tofino" in output:
-            opts = output
-        print(opts)
+@pytest.fixture(scope='class')
+def config_env():
+    os.environ["UTILITIES_UNIT_TESTING"] = "1"
+
+    yield
+
+    os.environ["UTILITIES_UNIT_TESTING"] = "0"
+
+class TestShowPlatformBarefoot(object):
+    """
+        Note: `show platform barefoot`  Here we test that the utility is called
+        with the appropriate option(s). 
+    """
+    def test_barefoot(self):
+        with mock.patch('utilities_common.cli.run_command') as mock_run_command:
+            CliRunner().invoke(show.cli.commands['platform'].commands['barefoot'].commands['profile'], [])
+        assert mock_run_command.call_count == 1
+        mock_run_command.assert_called_with('show platform barefoot profile', display_cmd=False)
